74af865bc0e2e8b9bca17a5cd5128087f6c7f477
luisgthub <luiamaral2@gmail.com>
2024-07-28 14:07:19 -0300

    Created the Category Controller, Migration and The Category View

---
+++ BookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BookWeb.Data;
+using BookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
