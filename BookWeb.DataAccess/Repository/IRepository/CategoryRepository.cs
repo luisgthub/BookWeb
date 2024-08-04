ed02d75ed7dc2d05551087f7f6fbb90a54d27eaf
luis <luiamaral2@gmail.com>
2024-08-04 12:02:44 -0300

    Created the Generic repository interface and class, and category repository

---
+++ BookWeb.DataAccess/Repository/IRepository/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using BookWeb.DataAccess.Data;
+using BookWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWeb.DataAccess.Repository.IRepository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository 
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db):base(db)
+        {
+            _db=db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ BookWeb.DataAccess/Repository/IRepository/Repository.cs
@@ -0,0 +1,46 @@
+﻿using BookWeb.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWeb.DataAccess.Repository.IRepository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+            throw new NotImplementedException();
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+          
+        }
+
+    }
+}
